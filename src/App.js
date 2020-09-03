fce9a0d4a0782fda242127d4205548465a5bca53
Ryan Sukale <ryansukale@gmail.com>
2020-09-03 09:22:56 -0400

    Create a simple vertical strip of rectangles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import SimpleGsap from './components/SimpleGsap';
+import Heatmap from './components/Heatmap';
 
 function App() {
   return (
     <div className="App">
-      <SimpleGsap></SimpleGsap>
+      {/* <SimpleGsap></SimpleGsap> */}
+      <hr/>
+      <Heatmap />
     </div>
   );
 }
--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
+import * as d3 from "d3";
 
 const isSaturday = date => date.getDay() === 6;
 
@@ -24,12 +25,36 @@ function getFirstSunday(weeks, date) {
 }
 
 export default () => {
+  const gridRef = useRef();
+
   const weeks = 52;
   const today = new Date();
   const closestSaturday = getClosestSaturday(today);
   const firstSunday = getFirstSunday(weeks, closestSaturday);
 
+  const rows = weeks;
+  const cols = 7;
+
+  useEffect(() => {
+    const gridNode = gridRef.current
+    const svg = d3.select(gridNode)
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 300);
+
+    const data = [1, 2, 3, 4, 5, 6, 7];
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('width', 20)
+      .attr('height', 20)
+      .attr('fill', 'gray')
+      .attr('y', (d) => d * 22)
+  }, []);
+
   return (
-    <div></div>
+    <div ref={gridRef}>
+    </div>
   );
 }
