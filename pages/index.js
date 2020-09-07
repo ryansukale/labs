7d54bdf4b59309571d8fd040df9abfb8d7c922c2
Ryan Sukale <ryansukale@gmail.com>
2020-09-07 10:14:41 -0400

    Using react-scripts for unpublished and next for published pages

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Home(props) {
+export default () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <Link href="/lab-gsap">
+        <a className="text-lg font-aleo">Gsap Lab</a>
+      </Link>
+    </div>
   );
 }
