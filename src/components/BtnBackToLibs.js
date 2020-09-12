a472a222687d19eeb428215e77d72163d21eb58b
Ryan Sukale <ryansukale@gmail.com>
2020-09-12 13:39:25 -0400

    Parameterize all the heatmap variables

---
+++ src/components/BtnBackToLibs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BtnBackToLibs() {
+  return (
+    <div
+      className="mb-2 flex justify-center items-center h-24">
+      <a href="/" className="btn-outline btn-outline-purple"> &#8592; Back to labs</a>
+    </div>
+  );
+}
--- src/lib/viz/createHeatmap.js
+++ src/lib/viz/createHeatmap.js
@@ -1,39 +1,65 @@
 import * as d3 from "d3";
 
-const defaultColors = {
-  start: '#cacaca',
-  end: 'blue'
-};
+// const defaultColors = {
+//   start: '#cacaca',
+//   end: 'blue'
+// };
+
+const defaultOptions = {
+  colors: {
+    start: '#cacaca',
+    end: 'blue'
+  },
+  rows: 7,
+  cellWidth: 15,
+  cellHeight: 15,
+  cellSpacing: 1,
+  cellSpacingX: undefined,
+  cellSpacingY: undefined
+}
 
 export default function ({
   node,
   data,
-  colors = defaultColors
+  options
 }) {
+  const values = data.map(d => d.value);
+
+  const {
+    colors,
+    cellWidth,
+    cellHeight,
+    cellSpacing,
+    cellSpacingX,
+    cellSpacingY,
+    rows
+  } = {...options, ...defaultOptions};
+
+  const totalCellWidth = cellWidth + (cellSpacingX || cellSpacing);
+  const totalCellHeight = cellHeight + (cellSpacingY || cellSpacing);
+
+  const svgWidth = data.length/rows * totalCellWidth;
+  
   const svg = d3.select(node)
       .append('svg')
-      .attr('width', 900)
-      .attr('height', 300);
+      .attr('width', svgWidth)
+      .attr('height', rows * totalCellHeight);
 
-    const values = data.map(d => d.value);
+  const colorScale = d3.scaleLinear()
+    .domain([d3.min(values), d3.max(values)])
+    .range([colors.start, colors.end]);
 
-    const colorScale = d3.scaleLinear()
-      .domain([d3.min(values), d3.max(values)])
-      .range([colors.start, colors.end]);
+  const cellX = (_, index) => totalCellWidth * Math.floor(index/rows);
+  const cellY = (_, index) => totalCellHeight * Math.floor(index%rows);
 
-    const cellWidth = 16;
-    const cellHeight = 16;
-    const cellX = (_, index) => cellWidth * Math.floor(index/7);
-    const cellY = (_, index) => cellHeight * Math.floor(index%7);
-
-    svg.selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('rx', 2)
-      .attr('width', cellWidth - 3)
-      .attr('height', cellHeight - 3)
-      .attr('fill', d => colorScale(d.value))
-      .attr('x', cellX)
-      .attr('y', cellY);
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('rx', 2)
+    .attr('width', cellWidth)
+    .attr('height', cellHeight)
+    .attr('fill', d => colorScale(d.value))
+    .attr('x', cellX)
+    .attr('y', cellY);
 }
